7d54751d0cb2bc5e8e4a791ae3b96dbbbdc09fc0
Leif Ringstad <leif@symbolia.com>
2017-02-27 09:25:24 +0100

    Update gems and entrypoints

--- copy_common_files.sh
+++ copy_common_files.sh
@@ -2,4 +2,8 @@
 cp Gemfile* alpine/
 cp Gemfile* elastic_mysql_alpine/
 cp Gemfile* elastic_mysql_ubuntu/
+cp Gemfile* elastic_mysql_ubuntu_ruby_2_4/
+cp Gemfile* ruby_2_4/
 cp docker-entrypoint.sh alpine/
+cp docker-entrypoint.sh ruby_2_4/
+git commit -a -m 'Update gems and entrypoints'
